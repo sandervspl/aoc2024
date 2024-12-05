fc8bb037ceadca61f7f84c36e38508b5dda56b48
Sander Vispoel <sandervispoel@hotmail.com>
2024-12-05 10:34:15 +0100

    day 5/1

---
+++ days/5/1.ts
@@ -0,0 +1,44 @@
+/**
+ * https://adventofcode.com/2024/day/5
+ */
+import _ from 'lodash';
+
+import 'services/array';
+import 'services/math';
+import 'services/input-file';
+
+const data = inputFile(5, 'input').split('\n\n');
+const rules = data[0]!
+  .split('\n')
+  .map((r) => r.split('|'))
+  .map((r) => r.map(Number));
+const input = data[1]!.split('\n');
+
+let answer = 0;
+
+for (const i of _.range(input.length)) {
+  let valid = true;
+  const row = input[i]!.split(',').map(Number);
+
+  for (const j of _.range(row.length)) {
+    const first = row[j];
+    const second = row[j + 1];
+
+    if (second == null) break;
+
+    for (const rule of rules) {
+      // If the numbers are the inverse of a rule we know the row is invalid
+      if (rule[1] === first && rule[0] === second) {
+        valid = false;
+        break;
+      }
+    }
+  }
+
+  if (valid) {
+    const middle = row.middle();
+    answer += middle;
+  }
+}
+
+console.log(answer);
