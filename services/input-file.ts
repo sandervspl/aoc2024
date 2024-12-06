1422d9c4d1f52019adeb73c49d3cad51127da524
Sander Vispoel <sandervispoel@hotmail.com>
2024-12-06 12:42:24 +0100

    Update input-file.ts

--- services/input-file.ts
+++ services/input-file.ts
@@ -3,7 +3,7 @@ import path from 'node:path';
 
 (globalThis as any).inputFile = function inputFile(day: number, fileName = 'input') {
   const file = path.resolve('days', day.toString(), fileName);
-  const buffer = fs.readFileSync(file, 'utf8');
+  const buffer = fs.readFileSync(file, 'utf8').trimEnd();
 
   return buffer;
 };
